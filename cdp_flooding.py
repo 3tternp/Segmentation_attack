76fcc223382b12bf7fcc62bf7b07fe96ce80184f
astra <26215963+3tternp@users.noreply.github.com>
2025-06-06 15:13:01 +0545

    Create cdp_flooding.py

---
+++ cdp_flooding.py
@@ -0,0 +1,8 @@
+# cdp_flooding.py
+import os
+
+def cdp_flood():
+    return "CDP Flooding simulated. (Tool like Yersinia required for actual flooding.)"
+
+if __name__ == '__main__':
+    print(cdp_flood())
