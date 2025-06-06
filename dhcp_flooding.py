214dc958ad089ab5ff9accf81491ec7946bf9ac2
astra <26215963+3tternp@users.noreply.github.com>
2025-06-06 15:14:19 +0545

    Create dhcp_flooding.py

---
+++ dhcp_flooding.py
@@ -0,0 +1,8 @@
+# dhcp_flooding.py
+import os
+
+def dhcp_flood():
+    return "DHCP Flood simulated. (Tool like Yersinia or custom scapy script required.)"
+
+if __name__ == '__main__':
+    print(dhcp_flood())
