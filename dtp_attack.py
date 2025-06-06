a8acdf09f8a79fc9ea7510a0925f2f2aefcdeea9
astra <26215963+3tternp@users.noreply.github.com>
2025-06-06 15:14:40 +0545

    Create dtp_attack.py

---
+++ dtp_attack.py
@@ -0,0 +1,8 @@
+# dtp_attack.py
+import os
+
+def dtp_spoof():
+    return "DTP Attack simulated. Requires DTP negotiation spoof (Yersinia/Scapy)."
+
+if __name__ == '__main__':
+    print(dtp_spoof())
