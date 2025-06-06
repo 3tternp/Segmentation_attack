1e3fca646a856c178565bcc30a4cbb48a4dda590
astra <26215963+3tternp@users.noreply.github.com>
2025-06-06 15:13:29 +0545

    Create mac_flooding.py

---
+++ mac_flooding.py
@@ -0,0 +1,8 @@
+# mac_flooding.py
+import os
+
+def mac_flood():
+    return "MAC Flooding simulated. (Run using tools like macof for real test.)"
+
+if __name__ == '__main__':
+    print(mac_flood())
